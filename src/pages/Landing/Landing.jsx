329995f8f47d8c2f4a5d265cd9be6ca978fe8574
karthikgoud <karthikrv09@gmail.com>
2023-06-10 12:40:43 +0530

    feat - added landing page

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,46 @@
+import styles from "./Landing.module.css";
+
+import landing from "../../assets/images/landing-img1.png";
+
+console.log(styles);
+
+const Landing = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.introCont}>
+        <div className={styles.title}>
+          <span className={styles.textMy}>My</span> Website
+        </div>
+        <div className={styles.details}>
+          <div className={styles.textSmall}>
+            <span className={`${styles.textBig} ${styles.greyColor}`}>
+              FOLLOW
+            </span>{" "}
+            PEOPLE AROUND THE GLOBE
+          </div>
+          <div className={styles.textSmall}>
+            <span className={`${styles.textBig} ${styles.greyColor}`}>
+              CONNECT
+            </span>{" "}
+            WITH YOUR FRIENDS
+          </div>
+          <div className={styles.textSmall}>
+            <span className={`${styles.textBig} ${styles.greyColor}`}>
+              SHARE
+            </span>{" "}
+            WHAT YOU THINK
+          </div>
+        </div>
+        <div className={styles.joinCont}>
+          <button>Join Now</button>
+          <p>Already have an Account?</p>
+        </div>
+      </div>
+      <div className={styles.imgCont}>
+        <img src={landing} alt="landing-img" />
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
