5a515b2ce4f3236c8fed509f30d64e7dceb531be
karthikgoud <karthikrv09@gmail.com>
2023-06-10 12:50:22 +0530

    feat - added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Landing from "./pages/Landing/Landing";
+import { Routes, Route, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <header>
+        <NavLink to="/">Landing</NavLink>
+      </header>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </div>
   );
 }
